6feaefa4c3f731a9eb2add1596db0bccb6c36958
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2021-10-29 13:42:46 -0300

    Updated script src

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,7 +1,3 @@
 
 
-    var price1 = 5;
-    var price2 = 6;
-    var total = price1 + price2;
-    document.getElementbyId("demo").innerHTML =
-    "The total is: " + total;
+    document.write(carName1) or document.write(carName2)
