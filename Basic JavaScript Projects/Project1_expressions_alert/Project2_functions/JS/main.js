fbe04ed6b13951cba672edd4348c21691fe48b84
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2021-11-02 09:38:08 -0300

    Assignment #13

--- Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/Project2_functions/JS/main.js
@@ -1,10 +1,6 @@
 //Javascript Functions//
 
 function myFunction() { 
-    var x = document.getElementById("demo"); x.style.color ="red";
-}
-
-function myFunction() {
-    var y = document.getElementById("demo"); y.style.font ="italic bold 20px arial, serif";
-
+    document.getElementById("demo").style.color="red";
+    document.getElementById("demo").style.font="italic bold 20px arial, serif";
 }
