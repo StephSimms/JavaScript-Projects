83914979502cd0821022949f64ae4f0de9f6aeb3
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2021-11-08 10:33:07 -0400

    Assignment #26

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,6 @@
+//this is the document write method
+
+document.write("<h1>Hello World!</h1><p>Have a nice day</p>");
+document.getElementById("typeof").innerHTML = typeof {name:'Steph' , age:41} + "<br>" + typeof [1,2,3,4] + "<br>" + typeof null + "<br>" + typeof function my Func(){};
+
+
