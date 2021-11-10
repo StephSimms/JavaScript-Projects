044882b6b4e198baef64d997d8acf4440686cd3e
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2021-11-10 12:55:09 -0400

    Re-Submission Assignment 7

--- Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/JS/main.js
@@ -1,63 +1,32 @@
 //this is Assignment #39
 let carName = "Honda";
-myFunction();
+myGlobal();
 
-function myFunction() {
+function myGlobal() {
   document.getElementById("global").innerHTML = "I can display " + carName;
 }
 
-myFunction();
+myLocal();
 
-function myFunction() {
+function myLocal() {
   let x = "Tree";
   document.getElementById("local").innerHTML = typeof x + " " + x;
 }
 
-document.getElementById("local1").innerHTML = typeof x;
 
-function myFunction(p1, p2) {
-    return p1 * p2;
-}
-
-document.getElementById("function").innerHTML = myFunction(4,3);
-
-//this is Assignment #40
-function myFunction() {
-    document.getElementById("color").style.color = "blue";
-}
-
-const d = new Date ("November 5, 2021 01:15:00");
-    document.getElementById("hours").innerHTML=d.getHours();
-
-//Assignment #41
-if (new Date ("November 2, 2021 Date").getHours() < 12) {
-    document.getElementById("IF").innerHTML= "Hello There!";
-}
-
-//this is Assignment #42
-function myFunction() {
-    document.getElementById("field2").value = document.getElementById("field1").value;
-}
-
-const hour = new Date("October 31, 2021").getHours();
-    let greeting;
-
-    if (hour <12) {
-        greeting = "Good Day";
-    } else {
-        greeting = "Good evening";
-    }
-
-    document.getElementById("ifelse").innerHTML = greeting;
 
 //this is Assignment #43
-const time = new Date().getHours();
-    let salutation;
+
+function myTime() {
+    const time = new Date().getHours();
+    let greeting;
     if (time <8) {
         greeting = "Good Morning";
     } else if (time < 18) {
         greeting = "Good Day";
     } else {
         greeting = "Good Evening";
-    }
-    document.getElementById("elseif").innerHTML = salutation;
+    } 
+    console.log(salutation)
+    document.getElementById("elseif").innerHTML = greeting;
+}
--- Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/JS/main.js
@@ -27,3 +27,8 @@ function showSlides(n) {
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
 }
+
+//Next button
+function myFunction() {
+    alert("Hello");
+}
