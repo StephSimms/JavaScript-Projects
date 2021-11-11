b365c2404f582a447aafd16d18228eb7960859d5
Stephanie Simms <88897284+StephSimms@users.noreply.github.com>
2021-11-11 09:18:27 -0400

    Constant

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,3 +1,4 @@
+//this is a loop
 let text = ""
 let i = 0;
 while (i < 10 ) {
@@ -5,3 +6,28 @@ while (i < 10 ) {
     i++;
 }
     document.getElementById("while").innerHTML = text;
+
+//this is a string length property
+let text1 = " Good Day ";
+    document.getElementById("string").innerHTML = text1.length;;
+
+//this is a for loop
+let text2 = "";
+
+for (let a = 0; a < 5; a++) {
+  text2 += "The number is " + a + "<br>";
+}
+
+document.getElementById("forloop").innerHTML = text2;
+
+//this is an array
+const cars = ["Honda", "Acura", "Subaru"];
+    document.getElementById("array").innerHTML = cars;
+
+//this is Assignment #53
+const car = {type:"Honda", model:"Odyssey", color:"silver"};
+    car.owner = "Stephanie";
+
+function myCar() {
+    document.getElementById("Constant").innerHTML = "Car owner is " + car.owner;
+}
